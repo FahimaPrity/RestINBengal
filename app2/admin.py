f3c8fc5c8a014bf7d3592e95c79259bc4f735c37
fahimapreety <fahimapreety6@gmail.com>
2025-04-27 02:02:06 +0600

    Initial commit

---
+++ app2/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Room, Review  # Import Room and Review models
+
+admin.site.register(Room)
+admin.site.register(Review)
+
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Hotel Rest IN Bengal Website
