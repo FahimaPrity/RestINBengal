c3b48ab71e75db95128504c151a93cb4fd724555
mehrinRahman <22101205@uap-bd.edu>
2025-05-02 01:16:39 +0600

    mehrin4

--- app1/models.py
+++ app1/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 from app2.models import Room
+from django.db import models
+from django.contrib.auth.models import User
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -29,3 +31,13 @@ class Point(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - Earned: {self.points_earned}, Used: {self.points_used}"
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reviewer_name = models.CharField(max_length=100)
+    rating = models.IntegerField()  # 1 থেকে 5 পর্যন্ত রেটিং
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.reviewer_name} - Rating: {self.rating}"
